5274a4afc1bc65eee65ea58b106bc0f89e102744
Jordan Hitch <thinkin.arbys@gmail.com>
2018-12-27 23:31:15 -0800

    Fixed a typo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-if (typeof process !== 'undefined' && process.env.NODE_ENV === 'developmemt') {
+if (typeof process !== 'undefined' && process.env.NODE_ENV === 'development') {
     module.exports = require('./dev-dist');
 } else {
     module.exports = require('./dist');
